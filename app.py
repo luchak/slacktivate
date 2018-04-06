8e2c093bb8a78509fed7594646251c2e82190e3b
Matt Stanton <luchak@gmail.com>
2018-04-06 12:22:58 -0700

    Respond to Slack Events API challenge.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, Response, request
+import json
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/event', methods=['POST'])
+def handle_event():
+    event = request.json
+    if event['type'] == 'url_verification':
+        return Response(event['challenge'], mimetype='text/plain')
+    else:
+        print(event)
+        return None
